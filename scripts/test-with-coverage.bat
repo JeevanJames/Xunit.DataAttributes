9cb654048072b1f517a4b40df7d200cefd0f109d
Jeevan James <jeevanjj@live.com>
2019-08-19 21:52:36 +0530

    Misc changes
    Update logo details in csproj
    Update coverage script to output to .coverage folder

--- scripts/test-with-coverage.bat
+++ scripts/test-with-coverage.bat
@@ -1,2 +1,2 @@
 dotnet test --blame .\tests\Xunit.DataAttributes.Tests\Xunit.DataAttributes.Tests.csproj /p:CollectCoverage=true /p:Exclude=\"[xunit.*]*\" /p:CoverletOutputFormat=opencover
-reportgenerator -reports:.\tests\Xunit.DataAttributes.Tests\coverage.opencover.xml -targetdir:.\coverage
+reportgenerator -reports:.\tests\Xunit.DataAttributes.Tests\coverage.opencover.xml -targetdir:.\.coverage
